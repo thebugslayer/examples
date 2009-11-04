ab92752956f98550b3efc1cb24228e6070bd513e
zemian <thebugslayer@gmail.com>
2009-11-03 21:08:15 -0500

    renamed jruby-examples/jruby_call_java

--- jruby/script_java_name_crash/Test02.java
+++
@@ -1,6 +0,0 @@
-public class Test02 {
-  public void output() {
-    System.out.print("From Java");
-  }
-}
-
--- jruby/script_java_name_crash/Test02.rb
+++
@@ -1,6 +0,0 @@
-#require 'java'
-#require 'Test02.jar'
-
-test = Java::Test02.new()
-test.output
-
